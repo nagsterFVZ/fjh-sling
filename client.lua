0ff97284b4fda0238f3c3ded61c1f898c6553fe1
nagsterFVZ <nagsterza@gmail.com>
2021-09-24 15:26:29 +0200

    Fixed not getting weapons on start

--- client.lua
+++ client.lua
@@ -1,29 +1,34 @@
 local attached_weapons = {}
 local hotbar = {}
-local sling = "Back" 
+local sling = "Back"
+local playerLoaded = false
 
 Citizen.CreateThread(function()
   while true do
-      local me = PlayerPedId()
-      local items = QBCore.Functions.GetPlayerData().items
-      hotbar = { items[1], items[2], items[3], items[4], items[5], items[41] }
-      for slot, item in pairs(hotbar) do
-        if item ~= nil and item.type == "weapon" and Config.compatable_weapon_hashes[item.name] ~= nil then
-          local wep_model = Config.compatable_weapon_hashes[item.name].model
-          local wep_hash = Config.compatable_weapon_hashes[item.name].hash
-          
-          if not attached_weapons[wep_model] and GetSelectedPedWeapon(me) ~= wep_hash then
-              AttachWeapon(wep_model, wep_hash, Config.Positions[sling].bone, Config.Positions[sling].x, Config.Positions[sling].y, Config.Positions[sling].z, Config.Positions[sling].x_rotation, Config.Positions[sling].y_rotation, Config.Positions[sling].z_rotation)
+    if playerLoaded then
+        local me = PlayerPedId()
+        local items = QBCore.Functions.GetPlayerData().items
+        if items ~= nil then 
+          hotbar = { items[1], items[2], items[3], items[4], items[5], items[41] }
+          for slot, item in pairs(hotbar) do
+            if item ~= nil and item.type == "weapon" and Config.compatable_weapon_hashes[item.name] ~= nil then
+              local wep_model = Config.compatable_weapon_hashes[item.name].model
+              local wep_hash = Config.compatable_weapon_hashes[item.name].hash
+              
+              if not attached_weapons[wep_model] and GetSelectedPedWeapon(me) ~= wep_hash then
+                  AttachWeapon(wep_model, wep_hash, Config.Positions[sling].bone, Config.Positions[sling].x, Config.Positions[sling].y, Config.Positions[sling].z, Config.Positions[sling].x_rotation, Config.Positions[sling].y_rotation, Config.Positions[sling].z_rotation)
+              end
+            end
+          end
+          for key, attached_object in pairs(attached_weapons) do
+              if GetSelectedPedWeapon(me) == attached_object.hash or not inHotbar(attached_object.hash) then -- equipped or not in weapon wheel
+                DeleteObject(attached_object.handle)
+                attached_weapons[key] = nil
+              end
           end
         end
       end
-      for key, attached_object in pairs(attached_weapons) do
-          if GetSelectedPedWeapon(me) == attached_object.hash or not inHotbar(attached_object.hash) then -- equipped or not in weapon wheel
-            DeleteObject(attached_object.handle)
-            attached_weapons[key] = nil
-          end
-      end
-  Wait(500)
+    Wait(500)
   end
 end)
 
@@ -60,4 +65,9 @@ AddEventHandler('mg-weapon-sling:client:changeSling', function()
     else
       sling = "Back"
     end
+end)
+
+RegisterNetEvent('QBCore:Client:OnPlayerLoaded')
+AddEventHandler('QBCore:Client:OnPlayerLoaded', function()
+  playerLoaded = true;
 end)
